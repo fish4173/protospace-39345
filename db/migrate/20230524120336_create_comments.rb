1ac14148b508886197da6a1bd171556fb111fadd
fish4173 <uoji4173@gmail.com>
2023-05-26 11:10:54 +0900

    ユーザー名修正

--- db/migrate/20230524120336_create_comments.rb
+++ db/migrate/20230524120336_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.text :content
+      t.text :content ,null:false
       t.references :user,          null: false, default: nil,foreign_key: true
       t.references :prototype,     null: false, default: nil,foreign_key: true
       t.timestamps
